b7cc48fd1d0934c11339af6d678c3cf72ac9999e
mandem296 <75235392+mandem296@users.noreply.github.com>
2021-11-08 14:15:11 +0300

    Pentagon shape on OpenGl

---
+++ cmake-build-debug/CMakeFiles/pentagon.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pentagon.dir/main.cpp.obj"
+  "libpentagon.dll.a"
+  "pentagon.exe"
+  "pentagon.exe.manifest"
+  "pentagon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pentagon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
